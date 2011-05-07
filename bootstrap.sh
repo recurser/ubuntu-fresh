6c91f86079830e45d51bdf212f47c4c84a943070
Dave Perrett <dave@recurser.com>
2011-05-07 14:55:17 +1000

    Debugging.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,8 @@ if [ $(which git | wc -l) -eq 0 ]; then
 fi
 
 echo "Cloning fresh-ubuntu..."
-rm -Rf fresh-ubuntu
-git clone --recursive git://github.com/recurser/fresh-ubuntu.git
+#rm -Rf fresh-ubuntu
+#git clone --recursive git://github.com/recurser/fresh-ubuntu.git
 cd fresh-ubuntu
 
 echo "Installing..."
