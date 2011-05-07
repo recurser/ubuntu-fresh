233381d28271d8ae019a3b4f8a34857de4e5824f
Dave Perrett <dave@recurser.com>
2011-05-07 13:42:42 +1000

    Set up git and user account.

--- install.sh
+++ install.sh
@@ -22,6 +22,7 @@ function is_installed() {
     return 0
 }
 
+
 #--------------------------------------------------------------------
 #
 # R E A D   I N   I N I T I A L   U S E R   D A T A
@@ -41,3 +42,44 @@ while [ "$domain" == "" ]; do
     read domain
 done 
 confirm $domain
+
+# Read in the full name.
+while [ "$full_name" == "" ]; do
+    echo -n "What is your full name? "
+    read full_name
+done 
+confirm $full_name
+
+# Read in the full email address.
+while [ "$email" == "" ]; do
+    echo -n "What is your email address? "
+    read email
+done 
+confirm $email
+
+
+#--------------------------------------------------------------------
+#
+# S E T   U P   G I T   N A M E   A N D   E M A I L
+#
+#--------------------------------------------------------------------
+if [ $(git config --global --get user.name | wc -l) -eq 0 ]; then
+     git config --global user.name $full_name
+fi
+
+if [ $(git config --global --get user.email | wc -l) -eq 0 ]; then
+     git config --global user.name $email
+fi
+
+
+#--------------------------------------------------------------------
+#
+# S E T   U P   U S E R   A C C O U N T
+#
+#--------------------------------------------------------------------
+if [ $(grep $user /etc/passwd | wc -l) -eq 0 ]; then
+    echo "Adding user '$user'"
+    adduser $user
+fi;
+
+
